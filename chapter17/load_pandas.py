9237311982bd317a9d61a96366408115a8376b42
Pham Hoang Minh - AI Engineer Lead <minh.pham@ndsvn.vn>
2025-07-25 11:04:56 +0700

    done chapter17

---
+++ chapter17/load_pandas.py
@@ -0,0 +1,10 @@
+# python load_pandas.py cryptid.psv
+import sys, pandas
+
+def read_pandas(fname:str) -> pandas.DataFrame:
+  data = pandas.read_csv(fname, sep="|")
+  return data 
+
+if __name__ == "__main__":
+  data = read_pandas(sys.argv[1])
+  print(data.head(5))
