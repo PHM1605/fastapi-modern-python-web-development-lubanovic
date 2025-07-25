d3ea78de75ea90b6528c21ff1937574b5e792902
Pham Hoang Minh - AI Engineer Lead <minh.pham@ndsvn.vn>
2025-07-25 11:54:55 +0700

    updates

--- chapter17/model/creature.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, constr
-
-class Creature(BaseModel):
-    name: constr(min_length=1) 
-    country: str 
-    area: str 
-    description: str 
-    aka: str 
-    
--- chapter17/model/explorer.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, constr
-
-class Explorer(BaseModel):
-    name: constr(min_length=1) 
-    country: str 
-    description: str 
-    
